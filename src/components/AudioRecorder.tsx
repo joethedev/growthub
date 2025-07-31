96ec5415515dcbdbe9029cb5e7813b6122ddd1e3
Youssef Idrissi <ykhoulani@gmail.com>
2025-07-31 17:44:15 +0100

    feat: remove unused useEffect import from AudioRecorder component

--- src/components/AudioRecorder.tsx
+++ src/components/AudioRecorder.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function AudioRecorder() {
   const [recording, setRecording] = useState(false);
